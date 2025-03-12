daf1d3c25f3d663fd2f4a801bc216fcada51f19a
urviponda27 <urviponda27@gmail.com>
2025-03-12 20:58:08 +0530

    Added new route for setprofilepic

--- apps/Backend/routes/authRoutes.js
+++ apps/Backend/routes/authRoutes.js
@@ -301,3 +301,24 @@ router.post("/updateprofile", async (req, res) => {
     return res.status(500).json({ error: "Failed to update profile" });
   }
 });
+router.post("/setprofilepic", (req, res) => {
+  const { email, profilepic } = req.body;
+  Doctor.findOne({ email: email })
+    .then((savedUser) => {
+      if (!savedUser) {
+        return res.status(422).json({ error: "Invalid Credentials" });
+      }
+      savedUser.profilepic = profilepic;
+      savedUser
+        .save()
+        .then((user) => {
+          res.json({ message: "Profile picture updated successfully" });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
